31f5b2d11781afdd47460831ea23a4cc1779c214
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2017-03-13 17:55:16 +0900

    remove extra comments in start_shell.sh

--- start_shell.sh
+++ start_shell.sh
@@ -11,8 +11,6 @@ fi
 if [ "x$(docker-compose ps -q $1)" = "x" ]; then
   docker-compose build $1
   docker-compose up -d $1
-#  echo "Please execute 'docker-compose build $1; docker-compose up -d $1' before launching this script."
-#  exit 1
 fi
 docker exec -i -t `docker-compose ps -q $1` bash --login
 
