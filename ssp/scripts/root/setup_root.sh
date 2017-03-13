5722c4ced232c6ee6a910485531c5d22bf235a90
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2017-03-14 08:55:49 +0900

    fix timezone configuration

--- ssp/scripts/root/setup_root.sh
+++ ssp/scripts/root/setup_root.sh
@@ -4,6 +4,7 @@ set -eu
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y language-pack-ja lv
 echo "Asia/Tokyo" > /etc/timezone
+rm /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 update-locale LANGUAGE=ja_JP:ja LANG=ja_JP.UTF-8
 
